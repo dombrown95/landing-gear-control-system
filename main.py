928550f8dd1d08b200d62875996478e6e7849692
Dom Brown <dominicbrown4@hotmail.com>
2026-01-16 12:33:46 +0000

    split landing gear controller into module

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-from enum import Enum, auto
+from lgcs.controller import LandingGearController
 
-class GearState(Enum):
-    """Defines the discrete states of the landing gear control system."""
-    UP_LOCKED = auto()
-    TRANSITIONING_DOWN = auto()
-    TRANSITIONING_UP = auto()
-    DOWN_LOCKED = auto()
+def main():
+    controller = LandingGearController()
+    controller.command_gear_down()
+    controller.command_gear_up()
 
-class LandingGearController:
-    def __init__(self):
-        """Initialises the gear controller in the UP_LOCKED state."""
-        self.state = GearState.UP_LOCKED
-    
-    def log(self, message):
-        """Outputs a status message for the landing gear system state."""
-        print(f"[{self.state.name}] {message}")
-
-    def command_gear_down(self):
-        """Commands the landing gear system to retract if currently locked down."""
-        if self.state == GearState.UP_LOCKED:
-            self.state = GearState.TRANSITIONING_DOWN
-            self.log("Gear deploying")
-            self.state = GearState.DOWN_LOCKED
-            self.log("Gear locked down")
-        else:
-            self.log("Command rejected")
-
-    def command_gear_up(self):
-        """Commands the landing gear system to deploy if currently locked up."""
-        if self.state == GearState.DOWN_LOCKED:
-            self.state = GearState.TRANSITIONING_UP
-            self.log("Gear retracting")
-            self.state = GearState.UP_LOCKED
-            self.log("Gear locked up")
-        else:
-            self.log("Command rejected")
-
-controller = LandingGearController()
-controller.command_gear_down()
-controller.command_gear_up()
+if __name__ == "__main__":
+    main()
